3ad1944a434af14919ab9d7dff413dd618d429de
Thomas Skerbis <thomas.skerbis@mac.com>
2024-11-19 23:05:48 +0100

    Update index.php

--- pages/index.php
+++ pages/index.php
@@ -6,6 +6,5 @@ use rex_addon;
 use rex_be_controller;
 use rex_view;
 
-$package = rex_addon::get('quick_navigation');
-echo rex_view::title($package->i18n('quick_navigation_title'));
+$package = rex_addon::get('a11y_docs');
 rex_be_controller::includeCurrentPageSubPath();
