0c27dcbe363be7114df8d17f0bd935fd8c5c62e4
Thomas Skerbis <thomas.skerbis@mac.com>
2024-11-19 21:06:07 +0100

    Update index.php

--- pages/index.php
+++ pages/index.php
@@ -1,34 +1,40 @@
 <?php
 $addon = rex_addon::get('a11y_docs');
 
-$content = '
-<div class="a11y-docs">
-    <div class="docs-header">
-        <input type="text" 
-               class="docs-search form-control" 
-               placeholder="' . $addon->i18n('search') . '" 
-               data-search>
-    </div>
+// Seiteninhalt
+$content = '';
 
-    <div class="docs-container">
-        <nav class="docs-nav">
-            <div class="toc-content" id="toc">
-                <!-- TOC wird per JavaScript generiert -->
-            </div>
-        </nav>
-        
-        <main class="docs-content">
-            <div class="content-wrapper" data-content>';
-
-$content .= rex_markdown::factory()->parse(rex_file::get(rex_path::addon('a11y_docs', 'README.md')));
-
+// Suche einbinden
 $content .= '
-            </div>
-        </main>
-    </div>
+<div class="docs-header">
+    <input type="text" 
+           class="docs-search form-control" 
+           placeholder="' . $addon->i18n('search') . '" 
+           data-search>
 </div>';
 
+// Dokumentation einbinden
+$content .= '
+<div class="docs-container">
+    <nav class="docs-nav">
+        <div class="toc-content" id="toc">
+            <!-- TOC wird per JavaScript generiert -->
+        </div>
+    </nav>
+    
+    <main class="docs-content">
+        <div class="content-wrapper" data-content>';
+
+// README.md als Hauptinhalt einbinden
+$content .= rex_markdown::parseFile(rex_path::addon('a11y_docs', 'README.md'));
+
+$content .= '
+        </div>
+    </main>
+</div>';
+
+// Fragment ausgeben
 $fragment = new rex_fragment();
+$fragment->setVar('title', $addon->i18n('title'), false);
 $fragment->setVar('body', $content, false);
-$fragment->setVar('title', $addon->i18n('title'));
 echo $fragment->parse('core/page/section.php');
