5fc4a7877d4d6c5a528a42a9186d108051328fbf
Thomas Skerbis <thomas.skerbis@mac.com>
2024-11-20 09:03:16 +0100

    Update boot.php

--- boot.php
+++ boot.php
@@ -1,15 +1,17 @@
 <?php
 if (rex::isBackend() && rex::getUser()) {
-    // Basis-Assets
-    rex_view::addCssFile($this->getAssetsUrl('css/style.css'));
-    rex_view::addJsFile('https://cdnjs.cloudflare.com/ajax/libs/mark.js/8.11.1/mark.min.js');
+    $page = rex_be_controller::getCurrentPage();
     
-    // Mermaid für Diagramme
-    rex_view::addJsFile('https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.min.js');
-    
-    // Wir fügen ein init.js File hinzu für die Mermaid-Initialisierung
-    rex_view::addJsFile($this->getAssetsUrl('js/init.js'));
-    
-    // Haupt-Script
-    rex_view::addJsFile($this->getAssetsUrl('js/docs.js'));
+    // Assets nur auf der Dokumentations-Seite laden
+    if ($page === 'a11y_docs/cheatsheet') {
+        // Basis-Assets
+        rex_view::addCssFile($this->getAssetsUrl('css/style.css'));
+        rex_view::addJsFile('https://cdnjs.cloudflare.com/ajax/libs/mark.js/8.11.1/mark.min.js');
+        
+        // Mermaid für Diagramme
+        rex_view::addJsFile('https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.min.js');
+        
+        // Haupt-Script
+        rex_view::addJsFile($this->getAssetsUrl('js/docs.js'));
+    }
 }
